595c5e2459ce686f1b5c9225355de63dadd73da8
daihang1 <49648247+daihang1@users.noreply.github.com>
2019-08-29 18:12:47 +0800

    好好地

--- day18/src/com/itheima/Demo.java
+++ day18/src/com/itheima/Demo.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world 你好呀");
     }
 }
